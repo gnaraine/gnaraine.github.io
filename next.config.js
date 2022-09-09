6879db4858f9115b8b0670eb1a5c3918f6df3915
g.n <41154336+gnaraine@users.noreply.github.com>
2022-09-08 21:31:43 -0400

    test

--- next.config.js
+++ next.config.js
@@ -1,11 +1,10 @@
-module.exports = {
-  images: {
-    domains: ["images.ctfassets.net"],
-  },
-};
+
 // next.config.js
 const isProd = process.env.NODE_ENV === "production";
 
 module.exports = {
-  assetPrefix: isProd ? "/your-github-repo-name/" : "",
+  assetPrefix: isProd ? "/gnaraine/" : "",
+  images: {
+    domains: ["images.ctfassets.net"],
+  },
 };
