54bb5c211bcddbd48e0962b7341f5fd10287774c
g.n <41154336+gnaraine@users.noreply.github.com>
2022-09-10 00:16:34 -0400

    style

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 // This default export is required in a new `pages/_app.js` file.
 
-import "../styles/style.css";
+import "./style.css";
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
