59642c14d106565cacbaea1e11a9041002c9b7e0
g.n <41154336+gnaraine@users.noreply.github.com>
2022-11-15 17:52:24 -0500

    init

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+}
 
-// next.config.js
-const isProd = process.env.NODE_ENV === "production";
-
-module.exports = {
-  assetPrefix: isProd ? "/gnaraine.github.io/" : "",
-  images: {
-    domains: ["images.ctfassets.net"],
-  },
-  compiler: {
-    styledComponents: true,
-  }
-};
+module.exports = nextConfig
